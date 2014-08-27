b8afce9bd9b81f465f2a99c3f46507164430128a
L <liangyuxin.02@gmail.com>
2014-08-27 17:10:17 +0800

    Init project.

---
+++ GameServer/src/main/java/common/boot/GameServer.java
@@ -0,0 +1,7 @@
+package common.boot;
+
+public class GameServer {
+	public static void main(String[] args) {
+		System.out.println("GameServer started.");
+	}
+}
---
+++ GameServer/src/main/java/common/net/Net.java
@@ -0,0 +1,5 @@
+package common.net;
+
+public class Net {
+
+}
